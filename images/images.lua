fee4cdb28a71b4149d6a97b4e8459869eb02ff01
snowdev <ukbrkkkk@gmail.com>
2026-09-20 17:02:26 -0300

    Create images.lua

---
+++ images/images.lua
@@ -0,0 +1,18 @@
+-- src/images/images.lua
+-- All Roblox asset IDs (images, icons, and font) used by the UI.
+
+local Images = {
+	FONT = "rbxassetid://12187365364",
+	KEY = "",
+	SUBMIT = "",
+	LINK = "",
+	DISCORD = "rbxassetid://126843387725536",
+	GLOW = "rbxassetid://8992230677",
+	LOGO = "rbxassetid://103859712365480",
+	CLOSE_ICON = "rbxassetid://110786993356448",
+	MOON_ICON = "rbxassetid://83380517901735",
+	SHADOW = "rbxassetid://6014261993",
+	GAME_PLACEHOLDER = "rbxassetid://74584987850498",
+}
+
+return Images
